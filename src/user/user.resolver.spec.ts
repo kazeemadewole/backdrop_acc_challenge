5b829d9eecd23db1823985d668a7ef324a08d030
Kazeem Adewole <adebayoadewole858@gmail.com>
2023-04-14 17:07:31 +0100

    added test to user

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserBankInformationDto } from './dto/userbankinformation.dto';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
-
+  const userBankInfo = {
+    user_account_number: '0049795678',
+    user_bank_name: 'Guaranty trust bank',
+    user_account_name: 'adewole kazeem adebayo',
+  };
+  const fakeUserService: Partial<UserService> = {
+    verifyUserBankDetails: (userBankInfo: UserBankInformationDto) =>
+      Promise.resolve({
+        account_name: 'adewole kazeem adebayo',
+        account_number: '0049795678',
+        is_verified: true,
+      }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
+      providers: [
+        UserResolver,
+        {
+          provide: UserService,
+          useValue: fakeUserService,
+        },
+      ],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
@@ -14,5 +34,13 @@ describe('UserResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should test if user is verified', async () => {
+    try {
+      await resolver.verifyUser(userBankInfo);
+    } catch (e) {
+      expect(e).toBeInstanceOf(TypeError);
+    }
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BankService } from '../bank/bank.service';
 import { UserService } from './user.service';
+import { UserEntity } from './Entity/user.entity';
+import { Repository } from 'typeorm';
 
 describe('UserService', () => {
   let service: UserService;
-
+  let userRepostory: Repository<UserEntity>;
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(UserEntity);
+  const mockUserBankDetails = {
+    user_account_number: '0049795678',
+    user_bank_name: 'guaranty trust bank',
+    user_account_name: 'Adewole kazeem adebayo',
+  };
   beforeEach(async () => {
+    const fakeBankService: Partial<BankService> = {
+      getAccountNumberResolution: (acc_no: string, bank_name: string) =>
+        Promise.resolve({
+          account_name: 'ADEWOLE KAZEEM ADEBAYO',
+          account_number: '0049795678',
+        }),
+    };
+    const fakeUserRepository = {
+      findOne: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+      save: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: BankService,
+          useValue: fakeBankService,
+        },
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: fakeUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepostory = module.get<Repository<UserEntity>>(USER_REPOSITORY_TOKEN);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be defined', () => {
+    expect(userRepostory).toBeDefined();
+  });
+
+  it('verify user bank details', async () => {
+    await service.verifyUserBankDetails(mockUserBankDetails);
+    expect(userRepostory.save).toHaveBeenCalled();
+  });
+
+  it('verify leveinstenin distance greater than 2 if wrong name passed', async () => {
+    mockUserBankDetails.user_account_name = 'Adewole azeez akinleye';
+    try {
+      await service.verifyUserBankDetails(mockUserBankDetails);
+    } catch (e) {
+      expect(e).toBe('Names do not match, Levenshtein distance greater than 2');
+    }
   });
 });
